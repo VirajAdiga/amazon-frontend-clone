f1bbfbabd2ac949c5f86f69bfbb4e5b5b1388c0a
VirajAdiga <virajadigamaclub@gmail.com>
2023-08-08 20:34:34 +0530

    Add navbar for the page

--- amazon-frontend-clone/src/App.js
+++ amazon-frontend-clone/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./Components/NavBar/Navigation"
+import MainPage from './Components/HomePage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <MainPage />
     </div>
   );
 }
---
+++ amazon-frontend-clone/src/Components/HomePage/MainPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                MainPage
+            </div>
+         );
+    }
+}
+ 
+export default MainPage;
